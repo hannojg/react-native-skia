cdddea69cea8058e9eed52cc60230355bf84fa9f
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-05-11 17:20:15 +0200

    wip: example use rea dirty hack

--- example/src/Examples/SkottieExample.tsx
+++ example/src/Examples/SkottieExample.tsx
@@ -1,44 +1,45 @@
 import {
-  Easing,
   Skia,
   SkiaView,
   useDrawCallback,
-  useTiming,
+  useSharedValueEffect,
 } from "@shopify/react-native-skia";
 import { useEffect, useMemo, useRef } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 
 import HandsLottie from "../assets/Hands.json";
+import {
+  Easing,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
 
 export const SkottieExample = () => {
   const skottie = useMemo(() => Skia.Skottie(JSON.stringify(HandsLottie)), []);
 
-  const progress = useTiming(
-    {
-      from: 0,
-      to: 1,
-      loop: true,
-    },
-    {
-      duration: skottie.duration * 1000,
-      easing: Easing.linear,
-    }
-  );
+  const progress = useSharedValue(0);
+
+  useEffect(() => {
+    progress.value = withRepeat(
+      withTiming(1, {
+        duration: skottie.duration * 1000,
+        easing: Easing.linear,
+      }),
+      -1,
+      false
+    );
+  }, []);
 
   const onDraw = useDrawCallback((canvas, info) => {
-    skottie.seek(progress.current);
+    skottie.seek(progress.value);
     const rect = Skia.XYWHRect(0, 0, info.width, info.height);
     skottie.render(canvas, rect);
   });
 
-  const skRef = useRef<SkiaView>(null);
-  useEffect(() => {
-    skRef.current?.registerValues([progress]);
-  }, [progress]);
-
   return (
     <SafeAreaView style={styles.flex1}>
-      <SkiaView ref={skRef} style={styles.flex1} onDraw={onDraw} />
+      <SkiaView style={styles.flex1} onDraw={onDraw} mode="continuous" />
     </SafeAreaView>
   );
 };
